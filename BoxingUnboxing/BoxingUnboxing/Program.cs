3fff3dd1710c0214e13d10efd6f181e0dd9d6bba
Pranit Jaiswal <pranit.96.pj@gmail.com>
2016-09-18 02:16:34 +0530

    Add files via upload

---
+++ BoxingUnboxing/BoxingUnboxing/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+            object obj = x;//Boxing
+            int y = (int)obj;//Unboxing
+        }
+    }
+}
