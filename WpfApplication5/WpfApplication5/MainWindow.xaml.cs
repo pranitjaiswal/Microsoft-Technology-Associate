9ab8dd8d4369ae53e08451af6192b6f356cab77d
Pranit Jaiswal <pranit.96.pj@gmail.com>
2016-09-18 03:27:39 +0530

    Add files via upload

---
+++ WpfApplication5/WpfApplication5/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Data successfully submitted");
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            txtfname.Text = "";
+            txtmname.Text = "";
+            txtlname.Text = "";
+            txtage.Text = "";
+            txtgender.Text = "";
+            txtmno.Text = "";
+        }
+
+        private void txtgender_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
