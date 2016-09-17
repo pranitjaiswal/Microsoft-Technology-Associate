597a12df5e3bf70f7aca526cb7f6a1fb3ac6ce68
Pranit Jaiswal <pranit.96.pj@gmail.com>
2016-09-18 05:00:10 +0530

    Add files via upload

---
+++ WpfApplication10/WpfApplication10/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication10
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            fname.Text = "";
+            mname.Text = "";
+            lname.Text = "";
+            address.Document.Blocks.Clear();
+            mailid.Text = "";
+            mno.Text = "";
+            country.Text = "";
+            state.Text = "";
+            city.Text = "";
+           
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Binding fname = new Binding();
+            fname.Source = fname;
+            fname.Path = new PropertyPath("Text");
+            tarfname.SetBinding(TextBlock.TextProperty, fname);
+        }
+    }
+}
+
