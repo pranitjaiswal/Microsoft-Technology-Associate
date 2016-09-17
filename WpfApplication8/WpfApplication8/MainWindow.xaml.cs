171604d5364e2291cc4672ce8b2aa2a2f22e3268
Pranit Jaiswal <pranit.96.pj@gmail.com>
2016-09-18 03:58:04 +0530

    Add files via upload

---
+++ WpfApplication8/WpfApplication8/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Window obj1 = new Window();
+            obj1.Height = 500;
+            obj1.Width = 500;
+            obj1.Title = "Solid Colour Window";
+            SolidColorBrush scb = new SolidColorBrush(Colors.Red);
+            obj1.Background = scb;
+            obj1.Show();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Window obj2 = new Window();
+            obj2.Height = 200;
+            obj2.Width = 200;
+            obj2.Title = "LINEAR GRADIENT BRUSH WINDOW";
+            Point pstrt = new Point(0, 0);
+            Point pstop = new Point(1, 1);
+            LinearGradientBrush lgb = new LinearGradientBrush(Colors.Red,Colors.Black,pstrt,pstop);
+            obj2.Background = lgb;
+            obj2.Show();
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Window obj3 = new Window();
+            obj3.Height = 200;
+            obj3.Width = 200;
+            RadialGradientBrush rgb = new RadialGradientBrush(Colors.Black, Colors.Red);
+            obj3.Background = rgb;
+            obj3.Show();
+        }
+    }
+}
---
+++ WpfApplication8/WpfApplication8/csharpform.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication8
+{
+    /// <summary>
+    /// Interaction logic for csharpform.xaml
+    /// </summary>
+    public partial class csharpform : Window
+    {
+        public csharpform()
+        {
+            InitializeComponent();
+        }
+
+        private void main1_Loaded(object sender, RoutedEventArgs e)
+        {
+            StackPanel sp1 = new StackPanel();
+            StackPanel sp2 = new StackPanel();
+            Label lblName = new Label();
+            Label lblAge = new Label();
+            TextBox txtName = new TextBox();
+            TextBox txtAge = new TextBox();
+            Button btnsubmit = new Button();
+            Button btnclear = new Button();
+
+            sp1.Children.Add(lblName);
+            sp1.Children.Add(txtName);
+
+            sp2.Children.Add(lblAge);
+            sp2.Children.Add(txtAge);
+
+            main1.Children.Add(sp1);
+            main1.Children.Add(sp2);
+            main1.Children.Add(btnsubmit);
+            main1.Children.Add(btnclear);
+
+            lblName.Content = "Enter Name";
+            lblAge.Content = "Enter Age";
+            txtAge.Height = 25;
+            txtName.Height = 25;
+            btnsubmit.Content = "Submit";
+            btnclear.Content = "Clear";
+            btnclear.Height = 30;
+            btnsubmit.Height = 30;
+
+            Point pstrt = new Point(0, 0);
+            Point pstop = new Point(1, 1);
+            LinearGradientBrush lgb = new LinearGradientBrush(Colors.LightCoral, Colors.White, pstrt, pstop);
+            main1.Background = lgb;
+
+            Point pstrt1 = new Point(0, 0);
+            Point pstop1 = new Point(1, 1);
+            LinearGradientBrush lgb1 = new LinearGradientBrush(Colors.Red, Colors.White, pstrt1, pstop1);
+            btnsubmit.Background = lgb1;
+
+            RadialGradientBrush rgb = new RadialGradientBrush(Colors.SkyBlue, Colors.LightSlateGray);
+            btnclear.Background = rgb;
+
+        }
+    }
+}
