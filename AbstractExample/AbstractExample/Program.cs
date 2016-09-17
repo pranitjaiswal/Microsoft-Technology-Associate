38db4aae22fd6cd58d87b36b0245fd364b5a6934
Pranit Jaiswal <pranit.96.pj@gmail.com>
2016-09-18 02:02:34 +0530

    Add files via upload

---
+++ AbstractExample/AbstractExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractExample
+{
+    abstract class Animal
+    {
+        public abstract void FoodHabits();
+    }
+
+    class HerbAnimal:Animal
+    {
+
+        public override void FoodHabits()
+        {
+            Console.WriteLine("this is my herbi method");
+        }
+    }
+    class CarniAnimal : Animal
+    {
+        public override void FoodHabits()
+        {
+            Console.WriteLine("this is my carni method");
+            
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HerbAnimal a = new HerbAnimal();
+            a.FoodHabits();
+            CarniAnimal b = new CarniAnimal();
+            b.FoodHabits();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AbstractExample1/AbstractExample1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractExample1
+{
+    abstract class calculate
+    {
+        public abstract void cal(int x, int y);
+    }
+    class sum : calculate
+    {
+        public override void cal(int x, int y)
+        {
+            int a;           
+            a = x + y;
+            Console.WriteLine("sum is " + a);
+        }
+    }
+    class diff : calculate
+    {
+
+        public override void cal(int x, int y)
+        {
+            int b;
+            b = x - y;
+            Console.WriteLine("difference = " + b);
+        }
+    }
+    class mult : calculate
+    {
+
+        public override void cal(int x, int y)
+        {
+            int z;
+            
+            z = x * y;
+            Console.WriteLine("product= " + z);
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the 2 numbers : ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            sum s = new sum();
+            s.cal(x, y);
+            diff d = new diff();
+            d.cal(x, y);
+            mult m = new mult();
+            m.cal(x, y);
+            Console.ReadLine();
+        }
+
+    }
+}
